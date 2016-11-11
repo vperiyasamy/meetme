a1d9dfd83142febfc3737517068a8e8893b33ff0
Viswesh Periyasamy <vperiyasamy@wisc.edu>
2016-11-11 14:54:24 -0600

    added geomidpoint server code

--- server/src/server.py
+++ server/src/server.py
@@ -3,6 +3,7 @@
 from google.appengine.ext import db
 import webapp2
 import json # without this, you will have error 500
+import math
 
 MAIN_PAGE_HTML = """\
 <html>
@@ -10,6 +11,8 @@ MAIN_PAGE_HTML = """\
     <a href="storeavalue">StoreValue </a>
     </br>
     <a href="getvalue">QueryValue </a>
+    </br>
+    <a href="getmidpoint">GetMidpoint </a>
   </body>
 </html>
 """
@@ -87,7 +90,81 @@ class GetValue(webapp2.RequestHandler):
        <input type="hidden" name="fmt" value="html">
        <input type="submit" value="Get value">
     </form></body></html>\n''')
+   
+
+class GetMidpoint(webapp2.RequestHandler):
+
+  def get_midpoint(pairs):
+    cartesian = []
+	w = 0
+
+	# convert latitude and longitude to cartesian coordinates
+	for lat, lon in pairs:
+		# convert to radians
+        lat = lat * (math.pi) / 180 
+        lon = lon * (math.pi) / 180
+
+        # cartesian conversion
+        x = math.cos(lat) * math.cos(lon)
+        y = math.cos(lat) * math.sin(lon)
+        z = math.sin(lat)
+
+        # add to list of coordinates
+        xyz = (x, y, z)
+        cartesian.append(xyz)
+
+        w += 1  # w represents the weight of each location, we will take it to be 1 equally for all
+
+    # these will be the final cartesian coordinates of the midpoint
+	x = 0
+	y = 0
+	z = 0
+
+	#next, compute weighted average of each coordinate
+	for xyz in cartesian:
+        x += xyz[0]
+        y += xyz[1]
+        z += xyz[2]
+
+	x /= w
+	y /= w
+	z /= w
+
+	#lastly, convert back to latitude and longitude
+	lon = math.atan2(y, x)
+	hyp = math.sqrt( (x * x) + (y * y) )
+	lat = math.atan2(z, hyp)
+
+	lat = lat * 180 / math.pi
+	lon = lon * 180 / math.pi
+
+    # Python supports the creation of anonymous functions (i.e. functions that are 
+    # not bound to a name) at runtime, using a construct called "lambda". 
+    # http://www.secnetix.de/olli/Python/lambda_functions.hawk
+    # json for python:  http://docs.python.org/2/library/json.html
+    returnVal(self, lambda : json.dump(["VALUE", tag, value], self.response.out))
     
+    ## The above call to returnVal is equivalent to:
+    #self.response.headers['Content-Type'] = 'application/jsonrequest'
+    #json.dump(["VALUE", tag, value], self.response.out)
+    
+
+  def post(self):
+    # https://developers.google.com/appengine/docs/python/tools/webapp/requestclass
+    pairs = self.request.get('pairs')
+    self.get_midpoint(pairs)
+
+  def get(self):
+    self.response.out.write('''
+    <html><body>
+    <form action="/getvalue" method="post"
+          enctype=application/x-www-form-urlencoded>
+       <p>Tag<input type="text" name="tag" /></p>
+       <input type="hidden" name="fmt" value="html">
+       <input type="submit" value="Get value">
+    </form></body></html>\n''') 
+
+
 #### Utilty procedures for generating the output
 #### Handler is an appengine request handler.  writer is a thunk
 #### (i.e. a procedure of no arguments) that does the write when invoked.
@@ -108,5 +185,6 @@ application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/storeavalue', StoreAValue),
     ('/getvalue', GetValue),
+    ('/getmidpoint', GetMidpoint),
     ], debug=True)
 
