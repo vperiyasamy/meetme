a74f1ff1c887f27572b608872485e391f77186a8
Yefeng <yuan44@wisc.edu>
2016-11-14 16:09:44 -0600

    Update UI

---
+++ app/src/main/java/edu/wisc/meetme/LoginActivity.java
@@ -0,0 +1,33 @@
+package edu.wisc.meetme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Yaphet on 11/12/16.
+ */
+
+public class LoginActivity extends Activity{
+
+
+
+    public void goToHome(View view) {
+        Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(i);
+
+    }
+
+    public void goToRegister(View view) {
+        Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(i);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/edu/wisc/meetme/RegisterActivity.java
@@ -0,0 +1,18 @@
+package edu.wisc.meetme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Yaphet on 11/12/16.
+ */
+
+public class RegisterActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
