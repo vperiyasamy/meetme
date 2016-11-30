b770d13a830f7d15507dbc7baa9052d03e698ca7
Yefeng <yuan44@wisc.edu>
2016-11-30 17:01:24 -0600

    latest version
    11/30

--- app/src/main/java/edu/wisc/meetme/RegisterActivity.java
+++ app/src/main/java/edu/wisc/meetme/RegisterActivity.java
@@ -1,13 +1,23 @@
 package edu.wisc.meetme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Yaphet on 11/12/16.
  */
 
 public class RegisterActivity extends Activity {
+
+
+
+    public void goToLogin(View view) {
+        Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(i);
+
+    }
 
 
     @Override
