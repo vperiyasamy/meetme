f99083b788424a6ba17ad7c5746f6d087585f183
Viswesh Periyasamy <vperiyasamy@wisc.edu>
2016-12-17 17:48:03 -0600

    added hash table of categoryIDs to main activity

--- app/src/main/java/edu/wisc/meetme/MainActivity.java
+++ app/src/main/java/edu/wisc/meetme/MainActivity.java
@@ -28,6 +28,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Hashtable;
+
 import static edu.wisc.meetme.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity
@@ -39,6 +41,179 @@ public class MainActivity extends AppCompatActivity
 
     boolean isAvailable;
 
+    // user friendly string names of each category
+    private final String[] categories = {
+            "Afghan",
+            "African",
+            "Ethiopian",
+            "American",
+            "Asian",
+            "Burmese",
+            "Cambodian",
+            "Chinese",
+            "Cantoness",
+            "Dim Sum",
+            "Fujian",
+            "Hunan",
+            "Peking Duck",
+            "Shanghai",
+            "Szechuan",
+            "Taiwanese",
+            "Filipino",
+            "Himalayan",
+            "Hotpot",
+            "Indonesian",
+            "Japanese",
+            "Ramen",
+            "Soba",
+            "Sushi",
+            "Udon",
+            "Korean",
+            "Malay",
+            "Mongolian",
+            "Noodle House",
+            "Thai",
+            "Tibetan",
+            "Vietnamese",
+            "Australian",
+            "BBQ",
+            "Bakery",
+            "Breakfast",
+            "Bubble Tea",
+            "Buffet",
+            "Burgers",
+            "Cajun/Creole",
+            "Caribbean",
+            "Coffee",
+            "Creperie",
+            "Desserts",
+            "Frozen Yogurt",
+            "Ice Cream",
+            "Diner",
+            "Donuts",
+            "English",
+            "Falafel",
+            "Fast Food",
+            "Food Truck",
+            "French",
+            "Fried Chicken",
+            "German",
+            "Greek",
+            "Hot Dogs",
+            "Indian",
+            "North Indian",
+            "South Indian",
+            "Irish",
+            "Italian",
+            "Latin American",
+            "Mac & Cheese",
+            "Mediterranean",
+            "Mexican",
+            "Burritos",
+            "Tacos",
+            "Tex-Mex",
+            "Middle Eastern",
+            "Pizza",
+            "Portuguese",
+            "Poutine",
+            "Sandwiches",
+            "Seafood",
+            "Southern/Soul",
+            "Spanish",
+            "Sri Lankan",
+            "Steakhouse",
+            "Vegetarian/Vegan",
+            "Wings"
+    };
+
+    // array of category Id's to map in hash table
+    private final String[] categoryIds = {
+            "503288ae91d4c4b30a586d67", // Afghan
+            "4bf58dd8d48988d1c8941735", // African
+            "4bf58dd8d48988d10a941735", // Ethiopian
+            "4bf58dd8d48988d14e941735", // American
+            "4bf58dd8d48988d142941735", // Asian
+            "56aa371be4b08b9a8d573568", // Burmese
+            "52e81612bcbc57f1066b7a03", // Cambodian
+            "4bf58dd8d48988d145941735", // Chinese
+            "52af3a7c3cf9994f4e043bed", // Cantonese
+            "4bf58dd8d48988d1f5931735", // Dim Sum
+            "52af3aaa3cf9994f4e043bf0", // Fujian
+            "52af3afc3cf9994f4e043bf8", // Hunan
+            "52af3b463cf9994f4e043bfe", // Peking Duck
+            "52af3b593cf9994f4e043c00", // Shanghai
+            "52af3b773cf9994f4e043c03", // Szechuan
+            "52af3b813cf9994f4e043c04", // Taiwanese
+            "4eb1bd1c3b7b55596b4a748f", // Filipino
+            "52e81612bcbc57f1066b79fb", // Himalayan
+            "52af0bd33cf9994f4e043bdd", // Hotpot
+            "4deefc054765f83613cdba6f", // Indonesian
+            "4bf58dd8d48988d111941735", // Japanese
+            "55a59bace4b013909087cb24", // Ramen
+            "55a59bace4b013909087cb27", // Soba
+            "4bf58dd8d48988d1d2941735", // Sushi
+            "55a59bace4b013909087cb2a", // Udon
+            "4bf58dd8d48988d113941735", // Korean
+            "4bf58dd8d48988d156941735", // Malay
+            "4eb1d5724b900d56c88a45fe", // Mongolian
+            "4bf58dd8d48988d1d1941735", // Noodle House
+            "4bf58dd8d48988d149941735", // Thai
+            "52af39fb3cf9994f4e043be9", // Tibetan
+            "4bf58dd8d48988d14a941735", // Vietnamese
+            "4bf58dd8d48988d169941735", // Australian
+            "4bf58dd8d48988d1df931735", // BBQ
+            "4bf58dd8d48988d16a941735", // Bakery
+            "4bf58dd8d48988d143941735", // Breakfast
+            "52e81612bcbc57f1066b7a0c", // Bubble Tea
+            "52e81612bcbc57f1066b79f4", // Buffet
+            "4bf58dd8d48988d16c941735", // Burgers
+            "4bf58dd8d48988d17a941735", // Cajun / Creole
+            "4bf58dd8d48988d144941735", // Caribbean
+            "4bf58dd8d48988d1e0931735", // Coffee
+            "52e81612bcbc57f1066b79f2", // Creperie
+            "4bf58dd8d48988d1d0941735", // Deserts
+            "512e7cae91d4cbb4e5efe0af", // Frozen Yogurt
+            "4bf58dd8d48988d1c9941735", // Ice Cream
+            "4bf58dd8d48988d147941735", // Diner
+            "4bf58dd8d48988d148941735", // Donuts
+            "52e81612bcbc57f1066b7a05", // English
+            "4bf58dd8d48988d10b941735", // Falafel
+            "4bf58dd8d48988d16e941735", // Fast Food
+            "4bf58dd8d48988d1cb941735", // Food Truck
+            "4bf58dd8d48988d10c941735", // French
+            "4d4ae6fc7a7b7dea34424761", // Fried Chicken
+            "4bf58dd8d48988d10d941735", // German
+            "4bf58dd8d48988d10e941735", // Greek
+            "4bf58dd8d48988d16f941735", // Hot Dogs
+            "4bf58dd8d48988d10f941735", // Indian
+            "54135bf5e4b08f3d2429dfdd", // North Indian
+            "54135bf5e4b08f3d2429dfde", // South Indian
+            "52e81612bcbc57f1066b7a06", // Irish
+            "4bf58dd8d48988d110941735", // Italian
+            "4bf58dd8d48988d1be941735", // Latin American
+            "4bf58dd8d48988d1bf941735", // Mac & Cheese
+            "4bf58dd8d48988d1c0941735", // Mediterranean
+            "4bf58dd8d48988d1c1941735", // Mexican
+            "4bf58dd8d48988d153941735", // Burritos
+            "4bf58dd8d48988d151941735", // Tacos
+            "56aa371ae4b08b9a8d5734ba", // Tex-Mex
+            "4bf58dd8d48988d115941735", // Middle Eastern
+            "4bf58dd8d48988d1ca941735", // Pizza
+            "4def73e84765ae376e57713a", // Portuguese
+            "56aa371be4b08b9a8d5734c7", // Poutine
+            "4bf58dd8d48988d1c5941735", // Sandwiches
+            "4bf58dd8d48988d1ce941735", // Seafood
+            "4bf58dd8d48988d14f941735", // Southern / Soul
+            "4bf58dd8d48988d150941735", // Spanish
+            "5413605de4b0ae91d18581a9", // Sri Lankan
+            "4bf58dd8d48988d1cc941735", // Steakhouse
+            "4bf58dd8d48988d1d3941735", // Vegetarian / Vegan
+            "4bf58dd8d48988d14c941735"  // Wings
+    };
+
+    // category Hashtable
+    private Hashtable<String, String> categoryHash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +221,11 @@ public class MainActivity extends AppCompatActivity
         setContentView(activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // initialize hash table
+        for(int i = 0; i < categoryIds.length; i++) {
+            categoryHash.put(categories[i], categoryIds[i]);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
